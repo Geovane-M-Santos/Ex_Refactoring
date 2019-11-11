e34467faefc1efada39755147366fef4c5799ffe
MatheusGeo <geovane.santos@edu.unipar.br>
2019-11-11 20:25:30 -0300

    Primeiro Commit

---
+++ src/refactoring/Refactoring.java
@@ -0,0 +1,16 @@
+package refactoring;
+
+/**
+ *
+ * @author Geovane
+ */
+public class Refactoring {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
