02feb3cfa5aa0a8ad1e7e941a4c3e06dd0624ba3
MatheusGeo <geovane.santos@edu.unipar.br>
2019-11-18 22:40:11 -0300

    Substituindo Condições por Polimorfismo

--- src/refactoring/Customer.java
+++ src/refactoring/Customer.java
@@ -25,22 +25,24 @@ class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
+//        double totalAmount = 0;
+//        int frequentRenterPoints = 0;
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
+            
+//            frequentRenterPoints += each.getFrequentRenterPoints();
 
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t"
                     + String.valueOf(each.getCharge()) + "\n";
-            totalAmount += each.getCharge();
+//            totalAmount += each.getCharge();
 
         }
         //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints)
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints())
                 + " frequent renter points";
         return result;
     }
--- src/refactoring/Rental.java
+++ src/refactoring/Rental.java
@@ -23,35 +23,11 @@ class Rental {
     }
 
     double getCharge() {
-        double thisAmount = 0;
-        switch (this.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (this.getDaysRented() > 2) {
-                    thisAmount += (this.getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += this.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (this.getDaysRented() > 3) {
-                    thisAmount += (this.getDaysRented() - 3) * 1.5;
-                }
-                break;
-        }
-        return thisAmount;
+            return _movie.getCharge(_daysRented);
     }
+    
+    int getFrequentRenterPoints() {
+            return _movie.getFrequentRenterPoints(_daysRented);
+   }
 
-    public int getFrequentRenterPoints() {
-        // add frequent renter points
-
-        // add bonus for a two day new release rental
-        if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) && this.getDaysRented() > 1) {
-            return 2;
-        } else {
-            return 1;
-        }
-    }
 }
